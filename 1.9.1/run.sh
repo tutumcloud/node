0b2d1d1c8f92174359c14849b6dcff37b1955b92
bernardopericacho <bernardopericacho@gmail.com>
2015-11-23 13:02:11 +0100

    docker 1.9.1

---
+++ 1.9.1/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p /etc/tutum/agent/
+if [ ! -f "/etc/tutum/agent/tutum-agent.conf" ]; then
+    cat > /etc/tutum/agent/tutum-agent.conf <<EOF
+{
+    "CertCommonName":"${CERT_COMMON_NAME}",
+    "DockerHost":"${DOCKER_HOST_LISTEN}",
+    "TutumHost":"${TUTUM_HOST}",
+    "TutumToken":"${TUTUM_TOKEN}",
+    "TutumUUID":"${TUTUM_UUID}",
+    "DockerOpts": "-s vfs"
+}
+EOF
+fi
+
+echo "Using tutum-agent.conf:"
+cat /etc/tutum/agent/tutum-agent.conf
+
+mkdir -p /var/log/tutum && touch /var/log/tutum/docker.log && tail -F /var/log/tutum/docker.log &
+exec /usr/bin/tutum-agent -stdout "$@"
